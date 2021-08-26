a7c1e4345f71d1c86b47e384917c2d7af57c7449
Bruno Gabriel <bruno.glm_@hotmail.com>
2021-08-26 16:44:24 -0300

    implementing test for file upload on azure blob storage

--- storage/azure_blob_test.go
+++ storage/azure_blob_test.go
@@ -1,7 +1,9 @@
 package storage_test
 
 import (
+	"context"
 	"testing"
+	"time"
 
 	"github.com/mundipagg/boleto-api/config"
 	"github.com/mundipagg/boleto-api/mock"
@@ -15,7 +17,7 @@ func TestAzureBlob_Download(t *testing.T) {
 		config.Get().AzureStorageAccount,
 		config.Get().AzureStorageAccessKey,
 		config.Get().AzureStorageContainerName,
-		true,
+		config.Get().DevMode,
 	)
 	assert.Nil(t, err)
 
@@ -34,16 +36,6 @@ func TestAzureBlob_Download(t *testing.T) {
 			name: "Donwload successfully",
 			ab:   azureBlobInst,
 			args: args{
-				path:     "docker/Fallback",
-				filename: "teste.json",
-			},
-			want:    "secret",
-			wantErr: false,
-		},
-		{
-			name: "Donwload successfully",
-			ab:   azureBlobInst,
-			args: args{
 				path:     config.Get().AzureStorageOpenBankSkPath,
 				filename: config.Get().AzureStorageOpenBankSkName,
 			},
@@ -58,4 +50,29 @@ func TestAzureBlob_Download(t *testing.T) {
 			assert.NotNil(t, got)
 		})
 	}
+}
+
+func TestAzureBlob_Upload(t *testing.T) {
+	mock.StartMockService("9093")
+	clientBlob, err := storage.NewAzureBlob(
+		config.Get().AzureStorageAccount,
+		config.Get().AzureStorageAccessKey,
+		config.Get().AzureStorageContainerName,
+		config.Get().DevMode,
+	)
+
+	assert.Nil(t, err)
+
+	payload := `{"ID":"6127b37d36b0e8770b1668ae","uid":"7ce410cd-0682-11ec-852e-00059a3c7a00","secretkey":"7ce410cd-0682-11ec-852e-00059a3c7a00","publickey":"dad58ecd903ceda1ce6e479ff1e6fab399c8207dd000af127cbcdbb5cd3dfe8d","boleto":{"authentication":{},"agreement":{"agreementNumber":1103388,"agency":"3337"},"title":{"createDate":"2021-08-26T00:00:00Z","expireDateTime":"2021-08-31T00:00:00Z","expireDate":"2021-08-31","amountInCents":200,"ourNumber":14000000019047441,"instructions":"NÃO RECEBER APÓS O VENCIMENTO. O prazo de compensação de boleto é de até 3 dias úteis após o pagamento, o valor do limite poderá ficar bloqueado até o processamento.","documentNumber":"12345678901","boletoType":"OUT","BoletoTypeCode":"99"},"recipient":{"name":"Nome do Recebedor (Loja)","document":{"type":"CNPJ","number":"18727053000174"},"address":{"street":"Logradouro do Recebedor","number":"1000","complement":"Sala 01","zipCode":"00000000","city":"Cidade do Recebedor","district":"Bairro do Recebdor","stateCode":"RJ"}},"buyer":{"name":"Nome do Comprador (Cliente)","email":"comprador@gmail.com","document":{"type":"CPF","number":"11282705792"},"address":{"street":"Logradouro do Comprador","number":"1000","complement":"Casa 01","zipCode":"01001000","city":"Cidade do Comprador","district":"Bairro do Comprador","stateCode":"SC"}},"bankNumber":104,"requestKey":"5239ad4a-2a97-4d39-905a-2cc304971d11"},"bankId":104,"createDate":"2021-08-26T12:30:05.2479181-03:00","bankNumber":"104-0","digitableLine":"10492.00650 61000.100042 09922.269841 3 72670000001000","ourNumber":"14000000099222698","barcode":"10493726700000010002006561000100040992226984","links":[{"href":"http://localhost:3000/boleto?fmt=html\u0026id=6127b37d36b0e8770b1668ae\u0026pk=dad58ecd903ceda1ce6e479ff1e6fab399c8207dd000af127cbcdbb5cd3dfe8d","rel":"html","method":"GET"},{"href":"http://localhost:3000/boleto?fmt=pdf\u0026id=6127b37d36b0e8770b1668ae\u0026pk=dad58ecd903ceda1ce6e479ff1e6fab399c8207dd000af127cbcdbb5cd3dfe8d","rel":"pdf","method":"GET"}]}`
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(time.Second*20))
+	defer cancel()
+
+	err = clientBlob.Upload(
+		ctx,
+		config.Get().AzureStorageUploadPath,
+		"FileNameTest.json",
+		payload)
+
+	assert.Nil(t, err)
 }
